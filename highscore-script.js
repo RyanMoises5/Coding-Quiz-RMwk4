e3faa8c0d3ae0c4ba6e3df686915708887304987
Ryan M <ryan.a.moises@gmail.com>
2024-01-21 20:33:46 -0500

    High score table now populates automatically, CSS styling added

--- highscore-script.js
+++ highscore-script.js
@@ -1,3 +1,7 @@
+var backBtn = document.querySelector(".back");
+var clearBtn = document.querySelector(".clear");
+var listEl = document.querySelector("ul");
+
 var storedScores = JSON.parse(localStorage.getItem("Scores"));
 
 function compareByScore(a, b) {
@@ -7,15 +11,11 @@ function compareByScore(a, b) {
 var sorted = storedScores.sort(compareByScore);
 var highLow = sorted.reverse();
 
-var backBtn = document.querySelector(".back");
-var clearBtn = document.querySelector(".clear");
-
-
-
-
-
-
-
+for (let index = 0; index < highLow.length; index++) {
+    var liEl = document.createElement("li");
+    liEl.textContent = highLow[index].score + " - " + highLow[index].initials
+    listEl.appendChild(liEl);
+}
 
 backBtn.addEventListener("click", function () { // Reloads index.html
     window.location.href = "index.html";
@@ -24,4 +24,5 @@ backBtn.addEventListener("click", function () { // Reloads index.html
 clearBtn.addEventListener("click", function () {  // Clears the local storage of initials and scores
     scoreList = [];
     localStorage.setItem("Scores", JSON.stringify(scoreList));
+    window.location.href = "highscores.html";
 })
